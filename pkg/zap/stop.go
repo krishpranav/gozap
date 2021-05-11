5883001c92fbb014dca4127790508be8dbd022b6
krishpranav <krisna.pranav@gmail.com>
2021-05-11 15:23:43 +0530

    update function for stoping the tool

--- pkg/zap/stop.go
+++ pkg/zap/stop.go
@@ -1,6 +1,34 @@
 package zap
 
+import (
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
 // const values
 const AScanStop = "/JSON/ascan/action/stopAllScans/?"
 const SpiderStop = "/JSON/spider/action/stopAllScans/?"
 const AjaxSpiderStop = "/JSON/ajaxSpider/action/stopAllScans/?"
+
+func Stop(api, prefix string, options OptionsZAP) {
+	req, err := http.NewRequest("GET", api+prefix, nil)
+	if err != nil {
+		panic(err)
+	}
+	if options.APIKey != "" {
+		req.Header.Add("X-ZAP-API-Key", options.APIKey)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	log.WithFields(log.Fields{
+		"ZAP API": prefix,
+	}).Info("Stoped")
+
+	if err != nil {
+
+	}
+
+	defer resp.Body.Close()
+}
