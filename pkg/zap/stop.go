1464506bccd410d14a80c12111c97a44ea2761f6
krishpranav <krisna.pranav@gmail.com>
2021-05-11 15:16:15 +0530

    added imports, package, const values

---
+++ pkg/zap/stop.go
@@ -0,0 +1,6 @@
+package zap
+
+// const values
+const AScanStop = "/JSON/ascan/action/stopAllScans/?"
+const SpiderStop = "/JSON/spider/action/stopAllScans/?"
+const AjaxSpiderStop = "/JSON/ajaxSpider/action/stopAllScans/?"
