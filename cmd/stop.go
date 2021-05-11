4de36ea44c7ecbda183eb907781d8e44d2a1e991
krishpranav <krisna.pranav@gmail.com>
2021-05-11 13:35:41 +0530

    added function for stoping this tool

---
+++ cmd/stop.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+
+	zap "github.com/krishpranav/gozap/pkg/zap"
+	"github.com/spf13/cobra"
+)
+
+var stopCmd = &cobra.Command{
+	Use:   "stop (spider/ascan/ajaxspider/all)",
+	Short: "Stop Scanning",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("stop called")
+		if len(args) >= 1 {
+			if args[0] == "spider" {
+				zap.StopSpider(apiHosts, options)
+			} else if args[0] == "ascan" {
+				zap.StopActiveScan(apiHosts, options)
+
+			} else if args[0] == "ajaxspider" {
+				zap.StopAjaxSpider(apiHosts, options)
+
+			} else if args[0] == "all" {
+				zap.StopSpider(apiHosts, options)
+				zap.StopAjaxSpider(apiHosts, options)
+				zap.StopActiveScan(apiHosts, options)
+			}
+		} else {
+			fmt.Println("Please input scanning mode for stop")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stopCmd)
+}
