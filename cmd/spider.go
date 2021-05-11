7e7a86c30718231b182301752e5e9fa162691850
krishpranav <krisna.pranav@gmail.com>
2021-05-11 14:38:56 +0530

    added spider

---
+++ cmd/spider.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	zap "github.com/krishpranav/gozap/pkg/zap"
+	"github.com/spf13/cobra"
+)
+
+
+
+var spiderCmd = &cobra.Command{
+	Use:   "spider",
+	Short: "Add ZAP spider",
+	Run: func(cmd *cobra.Command, args []string) {
+		if URLs != "" {
+			zap.Spider(URLs, apiHosts, options)
+		} else {
+			fmt.Println("Please input --urls flag")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(spiderCmd)
+}
